4afc5d2a206b90f0b64d416c6188e86c533c67fd
Patrick <w.patrick.kelly@gmail.com>
2020-01-29 15:27:34 -0800

    Histogram function added, ununique words function added

--- histogram.py
+++ histogram.py
@@ -1,35 +1,39 @@
 """histogram.py"""
 word_histogram = {}
-with open('ununique-words.txt', 'r') as file:
-        #word_histogram = {}
-    #loop through file
+# ununique_words = 0
+def wordHistogram():
+    with open('ununique-words.txt', 'r') as file:
 
-    for line in file:
-        #loop through lines
-        for word in line.split(): #split into single words
-            # word_histogram = {}
-            single_word = word
-            word_histogram[single_word] = word_histogram.get(single_word, 0) + 1
+        #loop through file
+        for line in file:
+            #loop through lines
+            for word in line.split(): #split into single words
+                # word_histogram = {}
+                single_word = word
+                word_histogram[single_word] = word_histogram.get(single_word, 0) + 1
 
-print(word_histogram)
+    print(word_histogram)
 
-# unique Words
+    # unique Words
+def ununiqueWords():
+    ununique_words = 0
 
-ununique_words = 0
+    #loop through word_histogram for each word with a key of 1
+    for word in word_histogram.keys():
+        #if word frequency == 1
+        if word_histogram[word] == 1:
+            #increse counter
+            ununique_words += 1
+    print(f"There are {ununique_words} unique words.")
 
-for word in word_histogram.keys():
-    if word_histogram[word] == 1:
-        ununique_words += 1
-print(f"There are {ununique_words} unique words: ")
-
-#frequency
+    #frequency
 def frequency(word, histogram):
+    #return the frequency of the given "word" in the "histogram"
+    return word_histogram[word]
 
-    return histogram[word]
 
-
-print(frequency("the", word_histogram))
-
+print(f"the frequency of your word is {frequency('the', wordHistogram())}")
+ununiqueWords()
 
 
 
--- rearrange.py
+++ rearrange.py
@@ -5,8 +5,10 @@ from random import randint
 if __name__ == "__main__":
     args = sys.argv
 
+#words = one input argument
     words = args[1:]
 
+#for loop through words setting random_words is set to randint of the length of the input.
 for word in words:
     random_words = randint(0,len(words)-1)
     rand_item = words[random_words]
